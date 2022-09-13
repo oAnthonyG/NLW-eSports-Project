83ae095e24a144b5884bf4620c0a7098e1f513dc
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-09-13 19:50:01 -0300

    firts step finish: landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['Inter', 'sans-serif']
+      },
+      backgroundImage: {
+        galaxy: "url('/background.png')",
+        'nlw-gradient':'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
+        'game-radiant': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
   },
   plugins: [],
 }
