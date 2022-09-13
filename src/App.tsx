4e644ae3c88f25a417e3d4ccbd94e7b32dfb671f
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-09-13 18:06:36 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import './styles/main.css';
 
 function App() {
   return(
     <div>
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
-      <Button title="Hello Friend!"/>
+      <h1>teste</h1>
     </div>
   )
 }
